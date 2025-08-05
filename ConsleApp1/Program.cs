01226a3f64aa32185a445064ad3eee83f79e9a97
dan178 <dtjan177@learner.dvusd.org>
2025-08-05 08:11:59 -0700

    questions

--- ConsleApp1/Program.cs
+++ ConsleApp1/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Diagnostics.CodeAnalysis;
+
 Console.WriteLine("Hello, World!!");
+string name="bob";
+Console.WriteLine("what is your first and last name?");
+Console.WriteLine("what is your grade?");
+Console.WriteLine("whats your student id?");
+name = Console.ReadLine();
+
